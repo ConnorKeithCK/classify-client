54ade71ecdfea0e1bc9d9d3f97dca792ad11aaea
Connor Keith <connor@refractlabs.com>
2022-02-21 13:07:14 -0500

    login page basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
+import LoginPage from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Link to="/login">Login</Link>
+      <LoginPage />
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,38 @@
-import { Box, Container, makeStyles } from '@mui/material';
+import { Box, Container, Typography, TextField, Paper, Button } from '@mui/material';
 import React from 'react';
+import logo from '../imgs/ClassifyLogo.png';
 
 interface LoginPageProps {}
 
 const LoginPage: React.FC<LoginPageProps> = () => {
   return (
-    <Container>
-      <Box></Box>
+    <Container
+      sx={{
+        ml: 'auto',
+        mr: 'auto',
+        display: 'flex',
+        flexDirection: 'vertical',
+        height: 400,
+        width: 400
+      }}>
+      <Paper elevation={3} sx={{ width: 50, height: 50, position: 'absolute', left: 10, top: -30 }}>
+        <img src={logo} />
+      </Paper>
+      <Box
+        sx={{
+          border: '1px solid black',
+          boxShadow: '5px 5px 5px black',
+          justifyContent: 'space-between'
+        }}>
+        <Typography variant="h5">Login to Classify</Typography>
+        <Box sx={{ padding: 5 }}>
+          <TextField sx={{ mt: 5 }} variant="outlined" label="E-Mail Address" />
+          <TextField sx={{ mt: 5 }} variant="outlined" label="Password" />
+          <Button sx={{ mt: 5 }} variant="contained">
+            Login
+          </Button>
+        </Box>
+      </Box>
     </Container>
   );
 };
