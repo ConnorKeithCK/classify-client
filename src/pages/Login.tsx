0998470ed34fb72967a3865be97b9b86855192ac
Connor Keith <keithcs@mail.uc.edu>
2022-04-06 19:13:30 -0400

    updated style of login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,14 @@
-import { Box, Container, Typography, TextField, Paper, Button } from '@mui/material';
-import React from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import logo from '../imgs/ClassifyLogo.png';
+import {
+  Box,
+  Container,
+  Typography,
+  TextField,
+  Paper,
+  Button,
+} from "@mui/material";
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import logo from "../imgs/ClassifyLogo.png";
 
 interface LoginPageProps {}
 
@@ -11,37 +18,42 @@ const LoginPage: React.FC<LoginPageProps> = () => {
   return (
     <Container
       sx={{
-        ml: 'auto',
-        mr: 'auto',
-        height: 400,
+        ml: "auto",
+        mr: "auto",
+        height: 600,
         width: 400,
-        position: 'absolute',
+        position: "absolute",
         left: 0,
         right: 0,
-        top: 75
-      }}>
+        top: 75,
+      }}
+    >
       <Box
         sx={{
-          border: '1px solid black',
-          boxShadow: '5px 5px 5px black',
-          justifyContent: 'space-between',
-          display: 'flex',
-          flexDirection: 'column'
-        }}>
-        <Typography sx={{ fontFamily: 'cursive ', flex: 1 }} variant="h4">
-          Classify
-        </Typography>
+          border: "1px solid black",
+          boxShadow: "5px 5px 5px black",
+          justifyContent: "space-between",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h4">Classify</Typography>
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '60%',
-            margin: 'auto',
-            height: '60%'
-          }}>
+            display: "flex",
+            flexDirection: "column",
+            width: "60%",
+            margin: "auto",
+            height: "60%",
+          }}
+        >
           <TextField sx={{ mt: 5 }} variant="outlined" label="E-Mail Address" />
           <TextField sx={{ mt: 5 }} variant="outlined" label="Password" />
-          <Button sx={{ mt: 5 }} variant="contained" onClick={() => navigate('/home')}>
+          <Button
+            sx={{ mt: 5, mb: 5 }}
+            variant="contained"
+            onClick={() => navigate("/home")}
+          >
             Login
           </Button>
         </Box>
